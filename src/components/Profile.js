7e3947f2b925db1ad3c0c2d13cbf740e316d3a46
victhall <victoriahall.dev@gmail.com>
2023-03-06 10:00:40 -0500

    fix portfolio

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,7 +6,7 @@ export default function Profile() {
   return (
     <div className={classes['profile__container']}>
       <main className={classes['main__text']}>
-        <h1>Victoria Hall is a Frontend Developer based in Toronto, Canada
+        <h1>Victoria Hall is a Freelance Frontend Developer based in Toronto, Canada
           with a passion for thoughtful UI and engaging user experiences. Her work focuses on creating meaningful
           digital utilities for the material world.</h1>
           <Contact />
